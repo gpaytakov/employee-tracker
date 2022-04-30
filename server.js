c76f49661a55c4af9a9cd033d3fb996a9be9f92e
gpay <gpaytakov@gmail.com>
2022-04-30 00:18:56 -0500

    created develop branch with preliminary data

---
+++ server.js
@@ -0,0 +1,10 @@
+// inquirer is imported
+const inquirer = require('inquirer');
+// mysql2 is imported
+const mysql = require('mysql2');
+// console.table is imported
+const cT = require('console.table');
+
+
+
+

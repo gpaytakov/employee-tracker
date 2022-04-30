50692b37f600f64ea8bed50e7ef4cf56cbfba674
gpay <gpaytakov@gmail.com>
2022-04-30 09:26:27 -0500

    seeds file updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,32 @@
+INSERT INTO departments
+    (department_name)
+VALUES 
+    ('science'),
+    ('technology'), 
+    ('ela'),
+    ('math');
 
+INSERT INTO roles
+    (role_name)
+VALUES
+    ('chemistry'),
+    ('biology'),
+    ('english I'),
+    ('physics'),
+    ('debate'),
+    ('computer science'),
+    ('digital media');
 
-
+INSERT INTO employees
+    (first_name, last_name, role_name, manager)
+VALUES
+    ('James', 'Fraser', 'chemistry', 'John Doe'),
+    ('Jack', 'London', 'biology', 'John Doe'),
+    ('Robert', 'Bruce', 'physics', 'John Doe'),
+    ('Peter', 'Greenaway', 'english I', 'Ashley Rollins'),
+    ('Derek', 'Jarman', 'debate', 'Ashley Rollins'),
+    ('Paolo', 'Pasolini', 'computer science', 'Victoria Crane'),
+    ('Heathcote', 'Williams', 'digital media', 'Victoria Crane'),
+    ('Hubert', 'Crackanthorpe', 'math', 'Susan Hill'),
+    ('William', 'Carleton', 'math', 'Susan Hill'),
+    ('Gerald', 'Griffin', 'math', 'Susan Hill');
